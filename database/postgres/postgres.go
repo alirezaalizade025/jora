4a432913ab50e5bae67525284445222a38ad8afc
alirezaalizadeh <alirezaalizadeh025@gmail.com>
2023-05-29 23:25:55 +0330

    feat: add postgres & cli & migration

---
+++ database/postgres/postgres.go
@@ -0,0 +1,124 @@
+package postgres
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"nomasho/utility"
+
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	_ "github.com/lib/pq"
+)
+
+
+
+func CreateDatabase(databaseName string) {
+	host := utility.Getenv("DB_HOST", "127.0.0.1")
+	port := utility.Getenv("DB_PORT", "5432")
+	dbname := utility.Getenv("DB_NAME", "nomasho_db")
+	user := utility.Getenv("DB_USER", "postgres")
+	password := utility.Getenv("DB_PASSWORD", "postgres")
+	timezone := utility.Getenv("DB_TIMEZONE", "Asia/Tehran")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable TimeZone=%s", host, port, user, password, timezone)
+	DB, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// createDatabaseCommand := fmt.Sprintf("DROP DATABASE %s", databaseName)
+	// DB.Exec(createDatabaseCommand)
+
+	var databaseExists int64
+	DB.Raw("SELECT count(*) FROM pg_database where datname = ?", dbname).Count(&databaseExists)
+
+	if databaseExists == 0 {
+		createDatabaseCommand := fmt.Sprintf("CREATE DATABASE %s", databaseName)
+		DB.Exec(createDatabaseCommand)
+	}
+}
+
+
+type dbConn struct {
+	Conn *gorm.DB
+}
+
+var conn *dbConn
+
+func Connection() *dbConn {
+	if conn == nil {
+		host := utility.Getenv("DB_HOST", "127.0.0.1")
+		port := utility.Getenv("DB_PORT", "5432")
+		dbname := utility.Getenv("DB_NAME", "nomasho_db")
+		user := utility.Getenv("DB_USER", "postgres")
+		password := utility.Getenv("DB_PASSWORD", "postgres")
+		sslmode := utility.Getenv("DB_SSLMODE", "disable")
+
+		dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", host, port, dbname, user, password, sslmode)
+
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+			// Logger: logger.Default.LogMode(logger.Silent),
+			PrepareStmt: true,
+		})
+		CheckError(err)
+
+		return &dbConn{Conn: db}
+	}
+	return conn
+}
+
+
+
+func TestConnection() *gorm.DB {
+	host := utility.Getenv("DB_HOST", "127.0.0.1")
+	port := utility.Getenv("DB_PORT", "5432")
+	user := utility.Getenv("DB_USER", "postgres")
+	password := utility.Getenv("DB_PASSWORD", "postgres")
+	sslmode := utility.Getenv("DB_SSLMODE", "disable")
+	dbname := utility.Getenv("DB_NAME", "nomasho_test_db")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", host, port, user, dbname, password, sslmode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		// Logger: logger.Default.LogMode(logger.Silent),
+	})
+
+	CheckError(err)
+
+	return db
+}
+
+
+
+func CheckError(err error) {
+	if err != nil {
+		// sentry.CaptureException(errors.New(err.Error()))
+		// sentry.Flush(time.Second * 10)
+		panic(err)
+	}
+}
+
+
+
+func Paginate(r *http.Request) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		q := r.URL.Query()
+		page, _ := strconv.Atoi(q.Get("page"))
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize, _ := strconv.Atoi(q.Get("per_page"))
+		switch {
+		case pageSize > 30:
+			pageSize = 30
+		case pageSize <= 0:
+			pageSize = 30
+		}
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+
+
